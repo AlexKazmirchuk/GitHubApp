943181fe3336344e47ad425d5759b3fff762c679
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-09-16 09:56:52 +0300

    added RepoEntity and UserEntity

---
+++ app/src/main/java/com/alexkaz/githubapp/model/entities/ShortUserEntity.java
@@ -0,0 +1,43 @@
+package com.alexkaz.githubapp.model.entities;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ShortUserEntity {
+
+    @SerializedName("login")
+    private String login;
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("avatar_url")
+    private String avatarUrl;
+
+    public ShortUserEntity() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+}
